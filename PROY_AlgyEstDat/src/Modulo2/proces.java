a7d08412e278d8cfe4c21efc63f734d598e78ef3
jhostin3031 <jhanzcristobal@gmail.com>
2025-10-30 18:38:59 -0500

    get

---
+++ PROY_AlgyEstDat/src/Modulo2/proces.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package Modulo2;
+
+/**
+ *
+ * @author USER
+ */
+public class proces {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int dato;
+        int num;
+        System.out.println("Ingrese el numero: ");
+    }
+    
+}
